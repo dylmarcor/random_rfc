ad39c99623e28dea64fcd999b0542d7de80d8984
Dylan Corbett <dylancorbett@icloud.com>
2022-06-15 17:28:30 -0700

    add RFC test case

--- test/test_rfc_generator.py
+++ test/test_rfc_generator.py
@@ -8,7 +8,16 @@ import ..src.random_rfc.py
 class TestRfc(unittest.TestCase):
 
     def test_rfc_generator(self):
-        pass
+        """
+        Test that a correct URL is generated
+        """
+
+        # Test for txt type
+        http = "https://www.rfc-editor.org/rfc/rfc"
+        rand_num = 3452
+        form = 'txt'
+        url = rfc_generator(http, 3452, form)
+        self.assertEqual(url, "https://www.rfc-editor.org/rfc/rfc3452.txt")
 
 if __name__ = "__main__":
     unittest.main()
