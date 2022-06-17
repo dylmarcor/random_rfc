21c46faacc51dd4fdcc6fede5ec953d79a8ca069
Dylan Corbett <dylancorbett@icloud.com>
2022-06-17 16:37:54 -0700

    switch up to simpler program

--- src/random_rfc.py
+++ src/random_rfc.py
@@ -16,13 +16,17 @@ import urllib.request
 rand_num = random.randint(0, 9260)
 
 # Create initial RFC url
-rfc = "https://www.rfc-editor.org/rfc/rfc" + str(rand_num)
+rfc = "https://www.rfc-editor.org/rfc/rfc" + str(rand_num) + '.txt'
+rfc_url = urllib.request.urlopen(rfc)
+
+for line in rfc_url:
+    print(line.decode("utf-8").rstrip())
 
 # Command intro - grab type of file wanted (default:txt)
 # print("What file would you like the random RFC in?")
 # print("Default is txt")
 # file_type = input()
-file_type = 'txt'
+# file_type = 'txt'
 
 def rfc_generator(rfc, f_format):
 
@@ -32,13 +36,8 @@ def rfc_generator(rfc, f_format):
         return rfc + '.txt'
 
 # From RFC Generator request URL and get results
-rfc_url = urllib.request.urlopen(rfc_generator(rfc, file_type))
-txt = []
-
-for line in rfc_url:
-    print(line.decode("utf-8").rstrip())
-    # print(line.rstrip('\n'))
-    # txt.append(line.rstrip('\n'))
+# rfc_url = urllib.request.urlopen(rfc_generator(rfc, file_type))
+# txt = []
 
 # Process output to 'less' command in shell
 subprocess.run(['less'], stdout=subprocess.PIPE, input=rfc_url.read().decode('utf-8'), shell= True, text=True).stdout
