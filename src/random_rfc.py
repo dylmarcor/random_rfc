a9a6abb2bba441f27eea346afd7a5419a88d1829
Dylan Corbett <dylancorbett@icloud.com>
2022-06-29 10:15:59 -0700

    add user_input

--- src/random_rfc.py
+++ src/random_rfc.py
@@ -15,13 +15,23 @@ import urllib.request
 # Generate random number for RFC url
 rand_num = random.randint(0, 9260)
 
-# Create initial RFC url
-rfc = "https://www.rfc-editor.org/rfc/rfc" + str(rand_num) + '.txt'
-rfc_url = urllib.request.urlopen(rfc)
+# Ask for desired direction
+user_input = input("Would you like a random RFC? y/n: ")
+
+if user_input == 'X' or 'x':
+    # Get user input for custome RFC
+    user_input = input("What number RFC would you like? Select between 1 - 9260: ")
+    rfc = "https://www.rfc-editor.org/rfc/rfc" + user_input + '.txt'
+    rfc_url = urllib.request.urlopen(rfc)
+else:
+    # Create initial RFC url
+    rfc = "https://www.rfc-editor.org/rfc/rfc" + str(rand_num) + '.txt'
+    rfc_url = urllib.request.urlopen(rfc)
 
 # Display RFC
 for line in rfc_url:
     print(line.decode("utf-8").rstrip())
+
 
 # TODO: Add specific RFC if desired function
 # TODO: Figure out how to pipe print to less shell command
